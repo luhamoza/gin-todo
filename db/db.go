0fa559ab1640face898501ad3cc871b40fdd1925
luhamoza <luhamoza@tostycedes.local>
2024-03-10 22:02:10 +0800

    feat: add insert query

--- db/db.go
+++ db/db.go
@@ -6,27 +6,27 @@ import (
 
 	_ "github.com/mattn/go-sqlite3"
 )
-var sqliteDB *sql.DB
+
+var SqliteDB *sql.DB
+
 func InitDb() {
 	var err error
-	sqliteDB, err = sql.Open("sqlite3","api.db")
+	SqliteDB, err = sql.Open("sqlite3", "api.db")
 	if err != nil {
 		panic("Could not initialize db")
 	}
-	sqliteDB.SetMaxOpenConns(25)
-	sqliteDB.SetMaxIdleConns(25)
-	sqliteDB.SetConnMaxLifetime(5*time.Minute)
+	SqliteDB.SetMaxOpenConns(25)
+	SqliteDB.SetMaxIdleConns(25)
+	SqliteDB.SetConnMaxLifetime(5 * time.Minute)
 
 	createTodoTable()
 }
 func createTodoTable() {
-	sqlStmt := `
-	CREATE TABLE IF NOT EXISTS todos (
+	sqlStmt := `CREATE TABLE IF NOT EXISTS todos (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		title TEXT NOT NULL,
-		completed BOOLEAN NOT NULL DEFAULT 0
-	)`
-	_, err := sqliteDB.Exec(sqlStmt)
+		completed BOOLEAN NOT NULL DEFAULT 0)`
+	_, err := SqliteDB.Exec(sqlStmt)
 	if err != nil {
 		panic("Could not create todos table")
 	}
--- models/todos.go
+++ models/todos.go
@@ -1,16 +1,31 @@
 package models
 
+import "github.com/luhamoza/gin-todo/db"
+
 type Todo struct {
-	ID          int
-	Title       string 
-	Status      bool
+	ID        int64
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
 }
 
-var todoList = []Todo{}
+var todoList []Todo
 
-func (t Todo) Save() {
-	todoList = append(todoList,t)
-} 
+func (t Todo) Save() error {
+	sqlStmt := `INSERT INTO todos (title,completed) VALUES (?,?)`
+	stmt, err := db.SqliteDB.Prepare(sqlStmt)
+	if err != nil {
+		panic("Could not prepare statement")
+		return err
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec(t.Title, t.Completed)
+	if err != nil {
+		panic("Could not execute statement")
+		return err
+	}
+	t.ID, err = result.LastInsertId()
+	return err
+}
 
 func GetAllTodos() []Todo {
 	return todoList
